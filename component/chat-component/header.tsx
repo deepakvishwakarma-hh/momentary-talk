d5787dc7ba60c4a0fcfeb81df16a81119c713f76
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-02-17 12:28:20 +0530

    update : Room's basic meta mainly for  dark theme

--- component/chat-component/header.tsx
+++ component/chat-component/header.tsx
@@ -1,27 +1,34 @@
 import { Box, Button, Flex, Grid, Input, Text } from "@chakra-ui/react"
 import Image from "next/image"
-
+import Head from "next/head"
 const Header = ({ query, showShare, setSetting, setShare, showSetting }) => {
 
     const settingOnClickHandler = () => { setSetting(!showSetting) }
     const shareOnClickHandler = () => { setShare(!showShare) }
 
     return (
-        <Flex justifyContent={"space-between"} alignItems={"center"} bg={"lightcyan"}>
-            <Text padding="1rem" fontSize='2xl' textAlign="center" fontFamily="Dosis" fontWeight="bold" >Momentary</Text>
-            <Text
-                display={['none', 'block']}
-            >ID: {query}</Text>
-            <Box>
-                <Button onClick={shareOnClickHandler} mx='2' bg={'none'} textTransform={'uppercase'}>
-                    <Image width={18} height={18} src="/share.svg" />
-                </Button>
-                <Button onClick={settingOnClickHandler} mx='2' bg={'none'} textTransform={'uppercase'}>
-                    <Image width={20} height={20} src="/settings.svg" />
-                </Button>
+        <>
+            <Head>
+                <title>Room -{query}</title>
+                <meta name="theme-color" content="#16161D" />
+            </Head>
+            <Flex justifyContent={"space-between"} alignItems={"center"} bg={"white"}>
+                <Text padding="1rem" fontSize='2xl' textAlign="center" fontFamily="Dosis" fontWeight="bold" >Momentary</Text>
+                <Text
+                    display={['none', 'block']}
+                >ID: {query}</Text>
+                <Box>
+                    <Button onClick={shareOnClickHandler} mx='2' bg={'white'} textTransform={'uppercase'}>
+                        <Image width={18} height={18} src="/share.svg" />
+                    </Button>
+                    <Button onClick={settingOnClickHandler} mx='2' bg={'none'} textTransform={'uppercase'}>
+                        <Image width={20} height={20} src="/settings.svg" />
+                    </Button>
 
-            </Box>
-        </Flex>
+                </Box>
+            </Flex>
+        </>
+
     )
 }
 

88ce28420f20cd1833c968652064e42731be97aa
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-02-20 10:14:09 +0530

    remove validation error

--- component/validation-system/validate-user-login.tsx
+++ component/validation-system/validate-user-login.tsx
@@ -1,9 +1,9 @@
-import { useLayoutEffect } from 'react'
+import { useEffect } from 'react'
 import React, { useState } from 'react'
 import UserNotLogin from './user-not-login'
 export default function Validator({ children }) {
     const [isUserLoggedIn, setUserCondition] = useState<boolean>(false)
-    useLayoutEffect(() => { (localStorage.getItem('token') !== null) ? setUserCondition(true) : setUserCondition(false) }, [])
+    useEffect(() => { (localStorage.getItem('token') !== null) ? setUserCondition(true) : setUserCondition(false) }, [])
     return (
         <>
             {(isUserLoggedIn) ? children : <UserNotLogin />}

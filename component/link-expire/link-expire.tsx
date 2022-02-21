2e5b0a31bbaccd868093d7b74d6b5d853532ef29
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-02-21 16:21:22 +0530

    update link-expire component :  change in ui

--- component/link-expire/link-expire.tsx
+++ component/link-expire/link-expire.tsx
@@ -1,11 +1,12 @@
-import { Alert, Center, Text } from "@chakra-ui/react"
+import { Alert, Center, Text, Button } from "@chakra-ui/react"
 import Link from "next/link"
 export default function LinkExpireFallback() {
     return (
         <Center bg="black" pos="fixed" width="100%" h="100%">
             <Alert display={'block'} bg={"blackAlpha.900"} color="white" maxWidth={['100%', '500px']} status='error'>
-                <Text color="red" fontSize={20}>Link Expired</Text>
-                <Text>This Link is Expired. Create new room or get another link from administator.  <Link href={"/"} >visit home</Link></Text>
+                <Text color="red" fontSize={25}>Link Expired</Text>
+                <Text>This Link is Expired. Create new room or get another link from administator. <br />  <Link href={"/"} >< Button fontWeight={100} colorScheme={'white'} my={5} variant={'outline'}>
+                    Visit home</Button></Link></Text>
             </Alert>
         </Center>
     )
